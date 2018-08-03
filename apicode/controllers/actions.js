71e0223f4755a9486e376d3d1a9a505b76796e50
Raj <Aravind.Raj@nordstrom.com>
2018-08-03 15:14:58 +0530

    Chnages within action methods-trying multiple me response

--- apicode/controllers/actions.js
+++ apicode/controllers/actions.js
@@ -2,28 +2,37 @@
 
 module.exports = {
 
-    call:function(functionName,req,res,next) //for a bit of readability and handling exception while calling
+    call: function (functionName, req, res, next) //for a bit of readability and handling exception while calling
     {
-        try
-        {
-
-            this[functionName](req,res,next);  //any function from this module is executed by functionName
+        try {
+            this[functionName](req, res, next);  //any function from this module is executed by functionName
         }
-        catch(error) //handling if action is not defined within api
+        catch (error) //handling if action is not defined within api
         {
-            res.json({fulfillmentText: "Error calling action : "+functionName+""});
+            res.json({ fulfillmentText: "Error calling action : " + functionName + "" });
         }
     },
 
 
-    actiontest:function(req,res,next){
+    actiontest1: function (req, res, next) {
 
-        return res.json({fulfillmentText: "This is the default message from hooktest for action actiontest"});
+        return res.json({ fulfillmentText: "This is the default message from hooktest for action actiontest" });
     },
 
-    actiontest2:function(req,res,next){
-
-        return res.json({fulfillmentText: "This is the default message from hooktest for action actiontest2"});
+    actiontest2: function (req, res, next) {
+        return res.json({
+            fulfillmentText:"Adada",
+            fulfillmentMessages: [
+                {
+                    text: {
+                        text: [
+                            "fgfgf",
+                            "Sosnasi"
+                        ]
+                    } 
+                }
+            ]
+        });
     }
 
 }
